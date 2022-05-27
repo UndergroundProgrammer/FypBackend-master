fa206e86dcd25388a8878b5dfad4059e7af552e7
UndergroundProgrammer <88035195+UndergroundProgrammer@users.noreply.github.com>
2022-05-27 00:43:20 -0700

    Update productApi.js

--- routes/api/productApi.js
+++ routes/api/productApi.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Product = require('../../models/Product');
+const {verifyToken} =   require("../../middlewares/authenticate");
 
 //cart
 
