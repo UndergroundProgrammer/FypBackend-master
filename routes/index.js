6c842b9fe81299b30c9eaada6088899d28cd7cad
UndergroundProgrammer <88035195+UndergroundProgrammer@users.noreply.github.com>
2022-06-24 10:17:00 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 router.get("/success", function (req, res, next) {
   res.clearCookie("cart");
   res.render("index", { title: "Congradulations! Payment Success" });
-  res.redirect("https://ar-medicare.vercel.app/");
 });
 
 router.get("/cancel", function (req, res, next) {
