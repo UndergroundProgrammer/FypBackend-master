4af04d24892bb4e6ef18adffbfbbf0ffbbeb6bd4
UndergroundProgrammer <88035195+UndergroundProgrammer@users.noreply.github.com>
2022-11-25 12:20:23 +0500

    some changes

--- models/Chat.js
+++ models/Chat.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
 
-const chatSchema = mongoose.Schema(
-  {
-    roomId : { type: String},
-    chatHistory:[
-        { 
-          room: { type: String },
-          message: { type: String },
-          author: { type: String },
-          time: { type: String },
-        }
-      ],
-  },
-);
-const chat = mongoose.model('Chat',chatSchema);
-module.exports =  chat;
+const chatSchema = mongoose.Schema({
+  roomId: { type: String },
+  chatHistory: [
+    {
+      room: { type: String },
+      message: { type: String },
+      author: { type: String },
+      time: { type: String },
+    },
+  ],
+});
+const chat = mongoose.model("Chat", chatSchema);
+module.exports = chat;
