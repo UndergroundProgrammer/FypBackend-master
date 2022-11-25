3faacc86d7e6b0b131444eb34bbd231aa3552928
UndergroundProgrammer <88035195+UndergroundProgrammer@users.noreply.github.com>
2022-11-25 17:18:33 +0500

    some fixes

--- src/app.js
+++ src/app.js
@@ -15,51 +15,11 @@ const respondantRouter = require("../routes/api/respondant");
 const orderRouter = require("../routes/api/orderApi");
 const paymentRouter = require("../routes/api/stripe");
 const chatRouter = require("../routes/api/ChatApi");
-var debug = require("debug")("fypbackend:server");
+
 const User = require("../models/User");
-var http = require("http");
-const { Server } = require("socket.io");
+
 var app = express();
 app.use(cors({ origin: true, credentials: true }));
-
-var server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
-
-  socket.on("send_message", async (data) => {
-    console.log("sending....");
-    socket.to(data.room).emit("receive_message", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
-});
-
-var port = "3000";
-/**
- * Create HTTP server.
- */
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(process.env.PORT || 3000);
-server.on("error", onError);
-server.on("listening", onListening);
 
 const dotenv = require("dotenv");
 
@@ -105,58 +65,4 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-mongoose.connect(process.env.DB_Connect, () => {
-  console.log("Connected");
-});
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-
-  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-  debug("Listening on " + bind);
-}
 module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,100 @@
+const mongoose = require("mongoose");
+var http = require("http");
+const { Server } = require("socket.io");
+var debug = require("debug")("fypbackend:server");
+const app = require("./app");
+
+mongoose.connect(process.env.DB_Connect, () => {
+  console.log("Connected");
+});
+
+var server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", async (data) => {
+    console.log("sending....");
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
+
+var port = "3000";
+/**
+ * Create HTTP server.
+ */
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(process.env.PORT || 3000);
+server.on("error", onError);
+server.on("listening", onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
+}
